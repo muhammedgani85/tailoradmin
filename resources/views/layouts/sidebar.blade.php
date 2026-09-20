289d952d96e25c1f0ac75e2fbe904bceca2a3bbd
muhammedgani85 <muhammedgn@gmail.com>
2026-09-20 12:13:53 +0530

    Changes

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -293,3 +293,5 @@
 <!-- Mobile Overlay -->
 <div x-show="$store.sidebar.isMobileOpen" @click="$store.sidebar.setMobileOpen(false)"
     class="fixed z-50 h-screen w-full bg-gray-900/50"></div>
+
+    
